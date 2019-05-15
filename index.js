c0f2ae46cba18e238e27e19b5e394c7c9ae4d59d
Vlad <aguretsvlad@gmail.com>
2019-05-15 19:42:19 +0300

    fin

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ require('./lib/getOptions')
 
 require('./lib/connection')
 
-const {rnd} = require('./utils/actionHelper')
 const User = require('./lib/user')
 
 var activeUsers = []
 
 async function main () {
   for (let i = 0; i < global._opts.connections; i++) {
-    activeUsers.push(new User({
-      name: nameGenerator(),
-      createdAt: new Date().valueOf(),
-      lifeTime: rnd(1 * 60) * 1000
-    }))
+    activeUsers.push(new User())
   }
-}
-
-function nameGenerator () {
-  return '_' + Math.random().toString(36).substr(2, 9)
 }
 
 main()
