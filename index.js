995c1a451b323d67c9de1b96266ab47c6a7e3693
Vlad <aguretsvlad@gmail.com>
2019-05-15 10:25:06 +0300

    comments

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const {_getOptsFromArgs} = require('./utils/processHelper')
 
 const _opts = _getOptsFromArgs()
 console.log(_opts);
+
+
+// TODO:
+// connecter to racer's ws logic
+// connections generator
+// make actions through ws
+// parrallel
