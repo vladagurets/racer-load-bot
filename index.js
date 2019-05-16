ac16d781c793dd1bedbeb10dad02a5f1a4d94516
Vlad <aguretsvlad@gmail.com>
2019-05-16 12:11:45 +0300

    rm unused

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ require('./lib/connection')
 
 const User = require('./lib/user')
 
-var activeUsers = []
-
 function main () {
-  for (let i = 0; i < global._opts.connections; i++) {
-    activeUsers.push(new User())
-  }
+  for (let i = 0; i < global._opts.connections; i++) new User()
 }
 
 main()
